4e16fffec2154a846289f903ad1ea474fda423d6
Mateusz Jaroszko <matt7269@protonmail.com>
2022-07-01 00:22:00 +0200

    Wyświetlanie zawartości tabeli samochody

--- Firma_Transportowa/Models/samochody.cs
+++ Firma_Transportowa/Models/samochody.cs
@@ -11,14 +11,29 @@ namespace Firma_Transportowa.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
     public partial class samochody
     {
-        public int id_samochodu { get; set; }
-        public string nr_rejestracyjny { get; set; }
-        public string ladownosc { get; set; }
-        public string wiek_pojazdu { get; set; }
-    
+        private int Id_samochodu;
+        private string Nr_rejestracyjny;
+        private string Ladownosc;
+        private string Wiek_pojazdu;
+
+        public int id_samochodu { get => Id_samochodu; set  { Id_samochodu = value; OnPropertyChanged(nameof(id_samochodu)); }
+}
+        public string nr_rejestracyjny { get => Nr_rejestracyjny; set { Nr_rejestracyjny = value; OnPropertyChanged(nameof(nr_rejestracyjny)); }
+}
+        public string ladownosc { get => Ladownosc; set { Ladownosc = value; OnPropertyChanged(nameof(ladownosc)); } }
+        public string wiek_pojazdu { get => Wiek_pojazdu; set { Wiek_pojazdu = value; OnPropertyChanged(nameof(wiek_pojazdu)); } }
+
         public virtual pracownicy_kierowcy pracownicy_kierowcy { get; set; }
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ Firma_Transportowa/ViewModels/samochodyViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Firma_Transportowa.View;
+using Firma_Transportowa.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firma_Transportowa.ViewModels
+{
+    public class samochodyViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private Samochody _samochody;
+
+        public Samochody samochody
+        {
+            get { return _samochody; }
+            set
+            {
+                _samochody = value;
+                OnPropertyChanged(nameof(samochody));
+            }
+        }
+        private ObservableCollection<samochody> _lstsamochody;
+
+        public ObservableCollection<samochody> Lstsamochody
+        {
+            get { return _lstsamochody; }
+            set
+            {
+                _lstsamochody = value;
+                OnPropertyChanged(nameof(Lstsamochody));
+            }
+        }
+        Firma_transportowaEntities5 samochodyEntities;
+        public samochodyViewModel()
+        {
+            samochodyEntities = new Firma_transportowaEntities5();
+            Loadsamochody();
+        }
+
+        private void Loadsamochody()
+        {
+            Lstsamochody = new ObservableCollection<samochody>(samochodyEntities.samochody);
+        }
+    }
+}
