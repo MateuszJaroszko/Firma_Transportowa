449b3b2498f80c5e5e2aab3b22ba7a643fbcad94
Mateusz Jaroszko <matt7269@protonmail.com>
2022-06-30 20:15:44 +0200

    Dodaj pliki projektów.

---
+++ Firma_Transportowa/View/Samochody.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Firma_Transportowa.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Samochody.xaml
+    /// </summary>
+    public partial class Samochody : Window
+    {
+        public Samochody()
+        {
+            InitializeComponent();
+        }
+    }
+}
